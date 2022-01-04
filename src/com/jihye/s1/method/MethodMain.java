f8c4b8cf7e9f49c8626329ae5fe5d2cfb4d4f0b2
keemzeehae <keemzeehae@naver.com>
2022-01-04 14:34:45 +0900

    make project

---
+++ src/com/jihye/s1/method/MethodMain.java
@@ -0,0 +1,11 @@
+package com.jihye.s1.method;
+
+public class MethodMain {
+
+	public static void main(String[] args) {
+		System.out.println("MainMethod Start");
+		System.out.println("MainMethod Finish");
+
+	}
+
+}
